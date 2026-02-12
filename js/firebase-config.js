00550b68bf8016e1bebca93e48485d2104680ae7
ymyoupre-sys <ym.youpre@gmail.com>
2026-02-12 18:34:30 +0900

    Update firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,15 @@
+// js/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCWy_BjB9tr02viCSfAx93qeJyX4G0e2iw",
+  authDomain: "keep-under-control.firebaseapp.com",
+  projectId: "keep-under-control",
+  storageBucket: "keep-under-control.firebasestorage.app",
+  messagingSenderId: "999632394190",
+  appId: "1:999632394190:web:085efbde0239f098c27d9f"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
